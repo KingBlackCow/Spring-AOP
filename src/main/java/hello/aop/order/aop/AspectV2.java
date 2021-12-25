b9186f292872946cb7348c6d09b94a4684ec91eb
KingBlackCow <sgs1159@naver.com>
2021-12-25 21:02:58 +0900

    포인트컷 분리

--- src/main/java/hello/aop/order/aop/AspectV2.java
+++ src/main/java/hello/aop/order/aop/AspectV2.java
@@ -1,18 +1,22 @@
 package hello.aop.order.aop;
-
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.springframework.stereotype.Component;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Slf4j
 @Aspect
-public class AspectV1 {
+public class AspectV2 {
+
     //hello.aop.order 패키지와 하위 패키지
-    @Around("execution(* hello.aop.order..*(..))")
+    @Pointcut("execution(* hello.aop.order..*(..))") //pointcut expression
+    private void allOrder(){} //pointcut signature
+    //반환타임은 무조건 void형
+
+    @Around("allOrder()")
     public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
-        log.info("[log] {}", joinPoint.getSignature()); //join point 시그니처 //어떤메서드가 호출되었는지
+        log.info("[log] {}", joinPoint.getSignature());
         return joinPoint.proceed();
     }
 }
